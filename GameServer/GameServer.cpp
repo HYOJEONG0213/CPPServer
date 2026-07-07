612812987be5b7b4cac1c8191edcecae637ee4bd
HYOJEONG0213 <fredflem@naver.com>
2026-07-07 21:24:36 +0900

    test: 스레드 생성 실습
    이런식으로 스레드 생성하면 되는구나~~
    
    t.join(): 해당 스레드가 joinable 상태이면 대상 스레드의 실행이 끝날 때까지 호출한 스레드를 대기 시킨다!

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,4 +2,20 @@
 #include <iostream>
 #include "CorePch.h"
 
-int main() { HelloWorld(); }
+#include <thread>
+
+void HelloThread() { cout << "thread hi\n"; };
+void HelloThread2(int32 num) { cout << num << "\n"; };
+
+int main()
+{
+	vector<std::thread> v;
+
+	for (int32 i = 0; i < 10; i++) { v.push_back(std::thread(HelloThread2, i)); }
+	for (int32 i = 0; i < 10; i++)
+	{
+		if (v[i].joinable()) v[i].join();
+	}
+
+	cout << "Hello Main \n";
+}
