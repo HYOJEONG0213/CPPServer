b2ff91bd80c484ae35c524f48a6caf933a14d403
HYOJEONG0213 <fredflem@naver.com>
2026-07-08 13:29:36 +0900

    test: Lock 기초 실습
    1. RAII 패턴(생성자에서 lock, 소멸자에서 unlock)
    - 템플릿 만들어 사용
    - STL에서 제공하는 std::lock_guard 사용
    - STL에서 제공하는 std::unique_lock 사용
    
    2. 수동으로 lock, unlock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,28 +4,50 @@
 
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-atomic<int32> sum = 0;
+vector<int32> v;
+mutex		  m;
 
-void Add()
+// RAII(Resource Acquisition Is Initialization)
+// 라이 패턴(생성자에서 잠그고 소멸자에서 풀기)
+template <typename T> class LockGuard
 {
-	for (int32 i = 0; i < 100000; i++) { sum.fetch_add(1); }
-}
+public:
+	LockGuard(T &m)
+	{
+		_mutex = &m;
+		_mutex->lock();
+	}
+	~LockGuard() { _mutex->unlock(); }
 
-void Sub()
+private:
+	T *_mutex;
+};
+
+void Push()
 {
-	for (int32 i = 0; i < 100000; i++) { sum.fetch_add(-1); }
+	for (int32 i = 0; i < 10000; i++)
+	{
+		// 방법 1,2,3 : RAII 패턴을 이용하여 lock과 unlock을 자동으로 처리
+		LockGuard<std::mutex> lockGuard(m); // 방법1 : 템플릿 만들기
+
+		std::lock_guard<std::mutex> lockGuard(m); // 방법2 : STL 제공
+
+		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock); // 방법3 : STL 제공(옵션 추가 제공)
+		uniqueLock.lock();											 // 방법3 : 수동으로 잠그고 풀기
+
+		m.lock(); // 방법4 : 수동으로 잠그고 풀기
+		v.push_back(i);
+		m.unlock();
+	}
 }
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << "\n";
-
-	std::thread t1(Add);
-	std::thread t2(Sub);
+	std::thread t1(Push);
+	std::thread t2(Push);
 	t1.join();
 	t2.join();
-	cout << sum << "\n";
+	cout << v.size() << "\n";
 }
