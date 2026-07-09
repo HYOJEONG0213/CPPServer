393196752adecaf8c3973fb371d987cc9569ad81
HYOJEONG0213 <fredflem@naver.com>
2026-07-09 22:15:43 +0900

    feat: Event 구현 #1
    커널에서 관리하는 오브젝트(Event)를 활용한 스레드 동기화 구현
    1. ::CreateEvent (이벤트 생성)
    2. ::SetEvent, ::WaitForSingleObject (상태 통보 및 대기)
    3. ::CloseHandle (리소스 반환)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,36 +6,59 @@
 #include <atomic>
 #include <mutex>
 #include "SpinLock.h"
+#include "windows.h"
 
-int32	 sum = 0;
-mutex	 m;
-SpinLock spinLock;
+mutex		 m;
+queue<int32> q;
+HANDLE		 handle;
 
-void Add()
+void Producer()
 {
-	for (int32 i = 0; i < 100000; i++)
+	while (true)
 	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum++;
-	};
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+
+		::SetEvent(handle); // Signal 상태로 변경
+
+		this_thread::sleep_for(10000000ms);
+	}
 }
 
-void Sub()
+void Consumer()
 {
-	for (int32 i = 0; i < 100000; i++)
+	while (true)
 	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
-	};
+		::WaitForSingleObject(handle, INFINITE); // Signal 상태가 될 때까지 대기
+		//::ResetEvent(handle);					 // Non-Signal 상태로 변경
+
+		unique_lock<mutex> lock(m);
+		if (q.empty() == false)
+		{
+			int32 data = q.front();
+			q.pop();
+			cout << data << "\n";
+		}
+	}
 }
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
+	// [커널 오브젝트]
+	// 이벤트 : 커널에서 관리하는 오브젝트
+	// 프로세스끼리 동기화 필요할때도 사용 가능
+	// Usage Count, Signal/Non-Signal(bool), Auto/Manual(bool)
+
+	// ::CreateEvent : (보안속성, 수동/자동 리셋(FALSE: AUTO RESET), 초기상태, 이름)
+	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+
+	thread t1(Producer);
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << "\n";
+	::CloseHandle(handle);
 }
