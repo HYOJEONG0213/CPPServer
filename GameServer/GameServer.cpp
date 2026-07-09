2a66283caf888ad3c9a926821f4bc441686462ad
HYOJEONG0213 <fredflem@naver.com>
2026-07-09 15:20:55 +0900

    feat: SpinLock 구현 #1
    1. 락 상태 체크 코드와 락의 상태를 바꿔주는 코드가 한번에(원자적으로) 일어나야한다! -> CAS(Compare and Swap) 방식인 compare_exchange_strong() 함수 사용
    2. 단점.. 경합 발생시 무한루프로 인한 cpu 점유율이 높아짐;;;

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,31 +5,65 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include "AccountManager.h"
-#include "Usermanager.h"
 
-void Func1()
+class SpinLock
 {
-	for (int32 i = 0; i < 100; i++) { UserManager::Instance()->ProcessSave(); }
-}
-void Func2()
+public:
+	void lock()
+	{
+		// CAS (Compare And Swap) : 원자적 연산
+		bool expected = false;
+		bool desired = true;
+
+		// _locked==expected라면 expected = _locked, _locked = desired로 바꾸고 true 리턴 : (1)
+		// _locked!=expected라면 expected = _locked, false 리턴 : (2)
+
+		// _locked==false면 (1) 실행 후 잠긴상태로 만들고 true 리턴
+		// _locked==true면 (2) 실행 후 while문으로 빠져나오지 못하고 계속 반복
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			// expected 는 매번 바뀌므로 계속 초기화 필요
+			expected = false;
+		};
+	}
+	void unlock()
+	{
+		_locked.store(false); //_locked=false; 해도 되지만 atomic이므로 명시적으로 store로 한거임!
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+int32	 sum = 0;
+mutex	 m;
+SpinLock spinLock;
+
+void Add()
 {
-	for (int32 i = 0; i < 100; i++) { AccountManager::Instance()->ProcessLogin(); }
+	for (int32 i = 0; i < 100000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
+	};
 }
+
+void Sub()
+{
+	for (int32 i = 0; i < 100000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
+	};
+}
+
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+	thread t1(Add);
+	thread t2(Sub);
+
 	t1.join();
 	t2.join();
-	cout << "End!" << "\n";
 
-	// 또다른 순서 맞추기 방법!
-	mutex m1;
-	mutex m2;
-	std::lock(m1, m2); // 알아서 내부적으로 일관적 순서 정해 잠궈준다!
-
-	// adopt_lock : 이미 잠겼으니 나중에 소멸될 때 풀어줘라!
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g1(m2, std::adopt_lock);
+	cout << sum << "\n";
 }
