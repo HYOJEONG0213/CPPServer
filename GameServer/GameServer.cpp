0ff9898e7efc22aa902f954e144a13289f61e3b4
HYOJEONG0213 <fredflem@naver.com>
2026-07-13 17:26:42 +0900

    test: future 실습 #1
    간단한 스레드 사용하는 경우 비동기 처리하는 future 객체 사용하자 (lock은 투머치함)
    
    1. async 사용하는 방법: 즉시/지연해 작업 수행 후 결과값을 future로 반환
    2. promise 사용하는 방법: 작업 스레드 내부에서 데이터 직접 설정해 리턴
    3. packaged_task 사용하는 방법: 호출할 함수자체를 포장해 자동으로 future에 채워짐

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,22 +5,72 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
+
 #include "SpinLock.h"
-#include "windows.h"
 #include "Event.h"
 
-Event event;
+class TestClass
+{
+public:
+	int64 GetValue() { return 100; }
+};
+
+int64 Calculate()
+{
+	int64 sum = 0;
+	for (int32 i = 0; i < 100000; i++) { sum += i; }
+
+	return sum;
+}
+
+void PromiseWorker(std::promise<string> &&promise) { promise.set_value("Secret Message"); }
+
+void TaskWorker(std::packaged_task<int64(void)> &&task) { task(); }
 
 int main()
 {
-	// ::CreateEvent : (보안속성, 수동/자동 리셋(FALSE: AUTO RESET), 초기상태, 이름)
-	event.handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	{
+		// std::future 를 사용한 비동기 방식!
+		// 옵션 3개 (deferred, async, deferred | async)
+		std::future<int64> future = std::async(std::launch::async, Calculate);
 
-	thread t1(&Event::Producer, &event);
-	thread t2(&Event::Consumer, &event);
+		std::future_status status = future.wait_for(1ms);
+		if (status == future_status::ready) { cout << "ready \n"; }
 
-	t1.join();
-	t2.join();
+		int64 sum = future.get(); // future.get() 은 Calculate() 가 끝날 때까지 기다림
+	}
 
-	::CloseHandle(event.handle);
+	{
+		// 클래스 멤버 함수 호출하는 경우
+		TestClass		   test;
+		std::future<int64> future2 = std::async(std::launch::async, &TestClass::GetValue, test);
+	}
+
+	{
+		// promise 를 사용한 비동기 방식!
+		// 미래(std::future)에 결과물을 반환해줄거라는 약속하기
+		std::promise<string> promise;
+		std::future<string>	 future = promise.get_future(); // future : 메인 스레드
+
+		thread t(PromiseWorker, std::move(promise)); // PromiseWorker : 워커 스레드
+
+		string message = future.get();
+		cout << message << "\n";
+
+		t.join();
+	}
+
+	{
+		// packaged_task 를 사용한 비동기 방식!
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64>				future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << "\n";
+
+		t.join();
+	}
 }
