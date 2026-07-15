297f380f963aa9c2b1191f6dc30acb5be2e1642c
HYOJEONG0213 <fredflem@naver.com>
2026-07-15 23:07:31 +0900

    test: Thread Local Storage 실습
    1. thread_local 로 변수를 지정해 스레드마다 자신만 접근할 수 있는 공간을 만들어준다!

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,71 +10,28 @@
 #include "SpinLock.h"
 #include "Event.h"
 
-atomic<bool> flag;
-atomic<bool> ready;
-int32		 value;
+thread_local int32 LThreadId = 0;
 
-void Producer()
+void ThreadMain(int32 threadId)
 {
-	value = 10;
-	// 타입1
-	ready.store(true, memory_order::memory_order_release);
-	// -------- 절취선 -------
+	LThreadId = threadId;
 
-	// 타입2
-	ready.store(true);
-	std::atomic_thread_fence(memory_order::memory_order_release)
-}
-
-void Consumer()
-{
-	// -------- 절취선 -------
-	while (ready.load(memory_order::memory_order_acquire) == false) { ; }
-
-	cout << value << "\n";
+	while (true)
+	{
+		cout << LThreadId << "st Thread! \n";
+		this_thread::sleep_for(1s);
+	}
 }
 
 int main()
 {
-	// 복습용 코드
+	vector<thread> threads;
+
+	for (int32 i = 0; i < 10; i++)
 	{
-		{
-			flag = false;
-
-			flag.store(true, memory_order::memory_order_seq_cst);
-
-			bool val = flag.load(memory_order::memory_order_seq_cst);
-		}
-
-		{
-			// 아래처럼 하면  멀티 스레드에서 꼬일수있음(원자적으로 이뤄지지 않음)
-			// .exchange() 를 사용하자!
-			// bool prev = flag;
-			// flag = true;
-
-			bool prev = flag.exchange(true);
-		}
-
-		{
-			// CAS(Compare and Swap) 조건부 수정
-			bool expected = false;
-			bool desired = true;
-			flag.compare_exchange_strong(expected, desired); // expected가 false이면 desired로 바꾸고
-															 // 아니면 expected를 현재 flag값으로 바꿈
-
-			// flag.compare_exchange_weak(expected, desired); // weak : Spurious Failure(가짜 실패) 가능성 있음
-		}
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
 	}
 
-	// Memory Model (정책)
-	// Sequentially Consistent (seq_cst)
-	// Acquire Release (consume, acquire, release, acq_rel)
-	// Relaxed (relaxed)
-
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
+	for (thread &t : threads) { t.join(); }
 }
