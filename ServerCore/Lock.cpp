c85fd3aea9dfc68df4b9834e8f323437470147dc
HYOJEONG0213 <fredflem@naver.com>
2026-07-26 14:26:20 +0900

    feat: Lock 클래스 디버깅모드일때만 GDeadLockProfiler 검사 #4
    1. 디버깅모드일때만 GDeadLockProfiler 호출해 데드락 검사
    2. 데드락 원인 추적을 위해 Write/ReadLock, Write/ReadUnlock 시 락의 이름도 같이 받도록 설정

--- ServerCore/Lock.cpp
+++ ServerCore/Lock.cpp
@@ -1,9 +1,14 @@
 #include "pch.h"
 #include "Lock.h"
 #include "CoreTLS.h"
+#include "DeadLockProfiler.h"
 
-void Lock::WriteLock()
+void Lock::WriteLock(const char *name)
 {
+#if _DEBUG
+	GDeadLockProfiler->PushLock(name);
+#endif
+
 	// 동일한 스레드가 소유하고 있다면 무조건 성공해야함
 	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16; // 상위 16비트 추출
 	if (LThreadId == lockThreadId)
@@ -35,8 +40,12 @@ void Lock::WriteLock()
 	}
 }
 
-void Lock::WriteUnlock()
+void Lock::WriteUnlock(const char *name)
 {
+#if _DEBUG
+	GDeadLockProfiler->PopLock(name);
+#endif
+
 	// ReadLock 다 풀기 전에는 WriteUnlock 불가능.
 	if ((_lockFlag.load() & READ_COUNT_MASK) != 0) CRASH("INVALID_UNLOCK_ORDER");
 
@@ -44,8 +53,12 @@ void Lock::WriteUnlock()
 	if (lockCount == 0) _lockFlag.store(EMPTY_FLAG);
 }
 
-void Lock::ReadLock()
+void Lock::ReadLock(const char *name)
 {
+#if _DEBUG
+	GDeadLockProfiler->PushLock(name);
+#endif
+
 	// 동일한 스레드가 소유시 무조건 성공
 	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
 	if (LThreadId == lockThreadId)
@@ -70,7 +83,11 @@ void Lock::ReadLock()
 	}
 }
 
-void Lock::ReadUnlock()
+void Lock::ReadUnlock(const char *name)
 {
+#if _DEBUG
+	GDeadLockProfiler->PopLock(name);
+#endif
+
 	if ((_lockFlag.fetch_sub(1) & READ_COUNT_MASK) == 0) CRASH("MULTIPLE_UNLOCK");
 }
