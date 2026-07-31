9364d40a9206c6c0c544d6ad6339c4523e49233b
HYOJEONG0213 <fredflem@naver.com>
2026-07-31 18:01:01 +0900

    test: 소수판별법1 멀티스레드로 구현 (매번 소수 확인법)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -9,31 +9,52 @@
 
 #include "ThreadManager.h"
 #include "CoreMacro.h"
-#include "UserManager.h"
-#include "AccountManager.h"
+
+// 멀티스레드를 사용해 100만 이하의 소수 개수 구하기 : 정답 : 78498
+// 만 이하의 소수 개수 : 1229
+// 천 이하의 소수 개수 : 168
+atomic<int> ret;
+const int	MAX_NUMBER = 1000000;
+
+bool IsPrime(int num)
+{
+	if (num <= 1) return false;
+	if (num == 2 || num == 3) return true;
+
+	for (int i = 2; i * i <= num; i++)
+	{
+		if ((num % i) == 0) return false;
+	}
+
+	return true;
+}
+
+int f1(int s, int e)
+{
+	int count = 0;
+	for (int i = s; i <= e; i++)
+	{
+		if (IsPrime(i)) count++;
+	}
+	return count;
+}
 
 int main()
 {
-	GThreadManager->Launch(
-		[=]
-		{
-			while (true)
-			{
-				cout << "UserThenAccount \n";
-				GUserManager.UserThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
-	GThreadManager->Launch(
-		[=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer \n";
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	vector<thread> threads;
 
-	GThreadManager->Join();
+	int coreCount = thread::hardware_concurrency();
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+		threads.push_back(thread(thread([start, end]() { ret += f1(start, end); })));
+	}
+
+	for (thread &t : threads) { t.join(); }
+
+	cout << ret << "\n";
 }
